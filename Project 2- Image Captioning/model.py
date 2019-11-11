cd208d020fb7bd2b44c30b401bd7d04c05b7d39b
Marwa Qabeel <marwa.qabeel138@gmail.com>
2019-11-11 21:20:54 +0200

    define model

--- Project 2- Image Captioning/model.py
+++ Project 2- Image Captioning/model.py
@@ -23,11 +23,34 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        embeddings = self.embed(captions[:,:-1])
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        #packed = pack_padded_sequence(embeddings, lengths, batch_first=True) 
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        sampled_ids = []
+        #inputs = inputs.unsqueeze(1)
+        for i in range(20):                                    # maximum sampling length
+            hiddens, states = self.lstm(inputs, states)        # (batch_size, 1, hidden_size), 
+            outputs = self.linear(hiddens.squeeze(1))
+            #print(outputs)# (batch_size, vocab_size)
+            predicted = outputs.max(1)[1]
+            #print('predicted',predicted)
+            #print(predicted.argmax())
+            sampled_ids.append(predicted.tolist()[0])
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)                       # (batch_size, 1, embed_size)
+        #print('sampled_ids',sampled_ids)
+        #print(sampled_ids.squeeze())
+        #sampled_ids = torch.cat(sampled_ids, 1)                # (batch_size, 20)
+        return sampled_ids#.squeeze()
